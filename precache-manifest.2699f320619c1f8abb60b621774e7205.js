d2e9a6b5830072b13efe32025ff315660f70787b
Alfan Fauzy <zhafhy@gmail.com>
2020-10-20 09:07:30 +0700

    Updates

---
+++ precache-manifest.2699f320619c1f8abb60b621774e7205.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfb64ad72016eed2e482e322b76b187",
+    "url": "/movie-search-react/index.html"
+  },
+  {
+    "revision": "023dafb1ac3bfff3f415",
+    "url": "/movie-search-react/static/css/2.3880dc7e.chunk.css"
+  },
+  {
+    "revision": "b11910a8905e046d68e0",
+    "url": "/movie-search-react/static/css/main.bb295494.chunk.css"
+  },
+  {
+    "revision": "023dafb1ac3bfff3f415",
+    "url": "/movie-search-react/static/js/2.570041ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-search-react/static/js/2.570041ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11910a8905e046d68e0",
+    "url": "/movie-search-react/static/js/main.ef184988.chunk.js"
+  },
+  {
+    "revision": "c5f9fbda35a8b62631af",
+    "url": "/movie-search-react/static/js/runtime-main.83ddd0ba.js"
+  },
+  {
+    "revision": "c0d8f65c5ba2071c7695d7d28d205eb6",
+    "url": "/movie-search-react/static/media/movie.c0d8f65c.png"
+  }
+]);
